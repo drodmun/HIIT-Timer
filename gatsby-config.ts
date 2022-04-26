486b4415681e9c04223e9f039257121d09ddc24b
Damian Rodriguez <damian.rodriguez@terramarnetworks.com>
2022-04-26 23:06:51 +0100

    Update config files for ENV variables, ignore JS and add GA

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import type { GatsbyConfig } from "gatsby";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+
 const path = require("path");
 
 const gatsbyRequiredRules = path.join(process.cwd(), "node_modules", "gatsby", "dist", "utils", "eslint-rules");
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
 
 // console.log('#################################################', path.resolve(process.cwd(), 'src/components'));
 const config: GatsbyConfig = {
@@ -37,6 +42,35 @@ const config: GatsbyConfig = {
         stages: ["develop"],
         extensions: ["ts", "tsx"],
         exclude: ["node_modules", "bower_components", ".cache", "public"]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GA_TRACKING_ID // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID" // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"]
+          // Defaults to https://www.googletagmanager.com
+          // origin: "https://drodmun-hiit-timer.netlify.app"
+        }
       }
     }
   ]
